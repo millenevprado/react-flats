741af09e39397dffa618d5b410a4404d324916f6
Millene Prado <millene_vprado@hotmail.com>
2022-09-14 13:07:45 -0300

    created marker component

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 
 class Flat extends Component {
+  handleClick = () => {
+    console.log(this);
+  }
+
   render() {
     return (
-      <div className="card"
-        style={{backgroundImage: `url(${this.props.imageUrl})`}}
-      >
+      <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url(${this.props.imageUrl})` }}>
         <div className="card-category">{this.props.price} {this.props.priceCurrency}</div>
         <div className="card-description">
           <h2>{this.props.name}</h2>
         </div>
-        <a className="card-link" href="#"></a>
+        <a className="card-link" href="#" onClick={this.handleClick}></a>
       </div>
     );
   }
