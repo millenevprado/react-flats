401622fdc630b983780f913c1ffb5887f1d34ac1
Millene Prado <millene_vprado@hotmail.com>
2022-09-14 11:12:27 -0300

    created app component

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className="flat-list"></div>
+        <div className="map-container"></div>
+      </div>
+    )
+  }
+}
+
+export default App;
